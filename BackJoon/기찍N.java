e17c1fd8a2ad1bf5831dc7f444fc0b2b5c9bca76
Doyeon Kim <60147434+doyeon111@users.noreply.github.com>
2022-04-10 01:32:29 +0900

    입력받은 n을 역순으로 출력하기

---
+++ BackJoon/기찍N.java
@@ -0,0 +1,22 @@
+package practice04;
+
+import java.util.Scanner;
+
+public class practice {
+
+	public static void main(String[] args) {
+
+		int n;
+
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+
+		for (int i = n; i >= 1; i--) {
+				System.out.println(i);
+
+			
+		}
+	}
+
+}
+ 
