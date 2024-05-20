6eab3be8791f4b028b4bbaed6cd1316ad31df036
dykim <dykim@bluedigm.com>
2024-05-20 13:29:15 +0900

    꼬마정민

---
+++ 꼬마정민.java
@@ -0,0 +1,14 @@
+package CodingTest.BackJoon;
+import java.util.Scanner;
+
+public class 꼬마정민 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long num = sc.nextLong();
+        long num2 = sc.nextLong();
+        long num3 = sc.nextLong();
+        System.out.println(num + num2 + num3);
+        sc.close();
+}
+}
